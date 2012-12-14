dd828474575d900e9b688186474b22bea35d79c6
肖攀 <spacexiao@163.com>
2012-12-13 22:59:47 -0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+
+  public String sayHello(){
+    return "Hello World!";
+  }
+
+  public static void main(String [] args){
+    System.out.println(new HelloWorld().sayHello());
+  }
+
+}
